3d75b1cb4a88f1cf944a4e85817802d9f6162b3d
Paul Billings <paulbillings101@yahoo.co.uk>
2020-10-23 21:15:32 +0100

    added the bookmarks dynamically from the local storage to the DOM

--- script.js
+++ script.js
@@ -35,6 +35,38 @@ function validate(nameValue, urlValue) {
   return true;
 }
 
+// build bookmarks DOM
+function buildBookmarks() {
+  // build items
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    // item
+    const item = document.createElement('div');
+    item.classList.add('item');
+    // close icon
+    const closeIcon = document.createElement('i');
+    closeIcon.classList.add('fas', 'fa-times');
+    closeIcon.setAttribute('title', 'Delete Bookmark');
+    closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+    // favicon / link container
+    const linkInfo = document.createElement('div');
+    linkInfo.classList.add('name');
+    // favicon
+    const favicon = document.createElement('img');
+    favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+    favicon.setAttribute('alt', 'Favicon');
+    // link
+    const link = document.createElement('a');
+    link.setAttribute('href', `${url}`);
+    link.setAttribute('target', '_blank');
+    link.textContent = name;
+    // append to bookmarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
+}
+
 // fetch bookmarks
 function fetchBookmarks() {
   // get bookmarks from localStorage if available
@@ -50,7 +82,7 @@ function fetchBookmarks() {
     ];
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
-  console.log(bookmarks);
+  buildBookmarks();
 }
 
 // handle data from form
